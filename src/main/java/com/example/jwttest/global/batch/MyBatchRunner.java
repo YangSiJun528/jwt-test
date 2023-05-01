70ecae26559263b33b46d8e635c4da9baab87576
YangSijun528 <yangsijun5528@gmail.com>
2023-05-02 00:00:05 +0900

    job 잘 동작하는 거 확인

--- src/main/java/com/example/jwttest/global/batch/MyBatchRunner.java
+++ src/main/java/com/example/jwttest/global/batch/MyBatchRunner.java
@@ -1,28 +1,38 @@
 package com.example.jwttest.global.batch;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobLocator;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MyBatchRunner implements CommandLineRunner {
-
-    private final Job job;
+    private final JobLocator jobLocator;
     private final JobLauncher jobLauncher;
 
     public void runJob() throws Exception {
+        Job job = jobLocator.getJob("simpleJob");
         JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDateTime("date", LocalDateTime.now())
                 .toJobParameters();
+        log.warn("job 실행");
 
         /* batch 수행 */
         jobLauncher.run(job, jobParameters);
     }
-
 
     @Override
     public void run(String... args) throws Exception {
--- src/main/java/com/example/jwttest/global/batch/job/RenewalMatchJobConfiguration.java
+++ src/main/java/com/example/jwttest/global/batch/job/RenewalMatchJobConfiguration.java
@@ -3,15 +3,22 @@ package com.example.jwttest.global.batch.job;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.StepContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -19,10 +26,17 @@ public class RenewalMatchJobConfiguration {
 
     private int chunkSize = 1;
 
+    // https://meteorkor.tistory.com/87
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
+    }
 
     @Bean
     public Job simpleJob(JobRepository jobRepository, Step testStep1) {
-        return new JobBuilder("jobParameterJob", jobRepository)
+        return new JobBuilder("simpleJob", jobRepository)
                 .start(testStep1)
                 .build();
     }
@@ -30,7 +44,7 @@ public class RenewalMatchJobConfiguration {
     @Bean
     public Step testStep1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager, Tasklet testTasklet1) {
         return new StepBuilder("testStep1", jobRepository)
-                .tasklet(testTasklet1)
+                .tasklet(testTasklet1, platformTransactionManager)
                 .transactionManager(platformTransactionManager)
                 .build();
     }
@@ -38,6 +52,9 @@ public class RenewalMatchJobConfiguration {
     @Bean
     public Tasklet testTasklet1() {
         return (stepContribution, chunkContext) -> {
+            StepContext stepContext = chunkContext.getStepContext();
+            Map<String, Object> jobParameters = stepContext.getJobParameters();
+            LocalDate date = (LocalDate) jobParameters.get("date");
             log.warn(">>>>>>tasklet");
             return RepeatStatus.FINISHED;
         };
